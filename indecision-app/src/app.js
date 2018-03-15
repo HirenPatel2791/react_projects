36f427cb69b07db7dc43d3a6b13e72759c395a0b
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-03-15 17:00:58 -0400

    adding babel

---
+++ indecision-app/src/app.js
@@ -0,0 +1,11 @@
+console.log('Apps is running');
+
+//JSX - JavaScript XML
+var template = <p> This is JSX from app.js</p>;
+
+//JSX - JavaScript XML
+
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
