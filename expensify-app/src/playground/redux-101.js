ff87894b3ed50d37b947b091b7268fa81ea73937
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-06-04 17:41:02 -0400

    reducers

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -21,8 +21,7 @@ const setCount = ({ count } = {}) => ({
     count
 });
 
-const store = createStore((state = { count: 0 }, action) => {
-    //console.log('running');
+const countReducers = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -44,7 +43,9 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state;
     }
-});
+};
+
+const store = createStore(countReducers);
 
 const unsubscribe = store.subscribe(()=>{
     console.log(store.getState());
--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 console.log();
 
 module.exports = {
-    entry: './src/playground/redux-101.js',
+    entry: './src/playground/redux-expensify.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
