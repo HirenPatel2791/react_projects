10c2ecbb9cbf72d4d24130861e6e65f26e8f03fb
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-06-04 14:24:53 -0400

    clean up

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        <p> EditExpensePage Page</p>
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+        <p> Editing the expense with id : {props.match.params.id}</p> 
     </div>
-);
+    );
+};
 
 export default EditExpensePage;
