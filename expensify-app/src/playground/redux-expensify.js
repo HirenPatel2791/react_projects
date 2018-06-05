21e8380ffef2c044ed88d84aad43c10d81a7faf9
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-06-05 10:49:17 -0400

    working with multiple reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,3 +1,55 @@
 import { createStore, combineReducers } from 'redux';
 
 console.log(0);
+
+
+//Expenses Reducer
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+//Filters Reducer
+const filtersReducerDefaultState = {
+    text : '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+
+const filtersReducers = (state = filtersReducerDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+//store Creation
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducers
+    })
+);
+
+console.log(store.getState());
+
+const demoState = {
+    expenses: [{
+        id: 'adfadfadfa',
+        description: 'Jan rent',
+        note: 'This was the final payment for that address',
+        amount: 54500,
+        createdAt: 0
+    }],
+    filters: {
+        text: 'rent',
+        sortBy: 'amount', //date or amount
+        startDate: undefined,
+        endDate: undefined
+    }
+};
