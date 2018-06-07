67d7b2022014bb20daf98bf46dae3c8ebe33716d
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-06-07 11:18:46 -0400

    react calender

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import moment from 'moment';
+import  { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
+const now = moment();
+console.log(now.format('MMM Do, YYYY '));
 class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calenderFocused: false
     };
 
     onDescriptionChange = (e) => {
@@ -22,6 +29,14 @@ class ExpenseForm extends React.Component {
     if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         } 
+    };
+
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    };
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calenderFocused: focused }));
     }
 
     render() {
@@ -40,6 +55,14 @@ class ExpenseForm extends React.Component {
                         value={this.state.amount}
                         placeholder="Amount"
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calenderFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={(day) => false}
                     />
                     <textarea
                         placeholder="Add a note for expense (optional)"
