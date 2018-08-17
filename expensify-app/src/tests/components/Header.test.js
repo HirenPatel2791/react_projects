ccb2dad8a0a344cbe3eba197270194d72e0c2b85
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-08-17 12:50:39 -0400

    setting up enzyme

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+//import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+//import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 // react-test-rendered
 
 test('Should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+    const wrapper = shallow(<Header />);
+    //expect(wrapper.find('h1').text()).toBe(' Expensify ');
+   // expect(toJSON(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
 });
