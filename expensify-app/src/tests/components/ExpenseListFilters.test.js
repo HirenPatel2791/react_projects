e65ddf55fd629706c32fbc4c0f84a72a39ef1d87
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-09-20 15:51:42 -0400

    tests done

--- expensify-app/src/tests/components/ExpenseListFilters.test.js
+++ expensify-app/src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
 
@@ -33,4 +34,51 @@ test('should render ExpenseListFilters with alt data correctly', () => {
       filters: altFilters
     });
     expect(wrapper).toMatchSnapshot();
-  });
+});
+
+test('should handle text change', () => {
+    const value = 'Sometext';
+    wrapper.find('input').simulate('change', {
+        target: { value }
+    });
+    expect(setTextFilter).toHaveBeenLastCalledWith(value);
+});
+
+test('should sort by date', () => {
+    wrapper.setProps({
+        filters: altFilters
+    });
+    const value = 'date';
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByDate).toHaveBeenCalled();
+});
+
+test('should sort by amount', () => {
+    const value = 'amount';
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByAmount).toHaveBeenCalled();
+});
+
+test('should handle date changes', () => {
+    const startDate = moment(0).add(5, 'day');
+    const endDate = moment(0).add(15, 'day');
+    wrapper.find('DateRangePicker').prop('onDatesChange')({
+        startDate,
+        endDate
+    });
+
+    expect(setStartDate).toHaveBeenCalledWith(startDate);
+    expect(setEndDate).toHaveBeenCalledWith(endDate);
+
+    
+});
+
+test('should handle date focus changes', () => {
+    const calendarFocused = 'endDate'
+    wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
+});
